9b010d1e0b4470f3c3f5515a07056a5432c0e0dc
LMc <llouismrc2@gmail.com>
2022-05-27 13:08:56 -0400

    main

--- lib/responsive/desktop_body.dart
+++ lib/responsive/desktop_body.dart
@@ -11,15 +11,14 @@ class DesktopBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Desktop'),   centerTitle: true,
+        title: const Text('Desktop'),
+        centerTitle: true,
       ),
-      body:
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
           children: [
             Expanded(
               child: ListView(
@@ -31,36 +30,116 @@ class DesktopBody extends StatelessWidget {
                   Expanded(
                     child: SizedBox(
                       height: MediaQuery.of(context).size.height,
-                      child: ListView(
-                      
-                            shrinkWrap: true,          
-                              children: [ 
-                                 BuildRow(child1: image[2],child: image[1],),
-                                 BuildRow(child: image[0],),
-                              ]),
+                      child: ListView(shrinkWrap: true, children: [
+                        BuildRow(
+                          child1: image[2],
+                          child: image[1],
+                        ),
+                        BuildRow(
+                          child: image[0],
+                        ),
+                      ]),
                     ),
                   )
                 ],
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                color: Colors.blue,
-                ),
-                width: 300,
-                child: Column(
-                  children: [
-                    
-                  ],
-                ),
-              )
-            )
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: const Color.fromARGB(255, 241, 227, 227),
+                  ),
+                  width: 300,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                       LinkButton(icon: SizedBox(child: Image.asset('assets/iconLink/iphone.png',
+                       width: 50,
+                       height: 50,
+                        fit: BoxFit.cover,)),
+                         text: 'Download for ios', ontap: () {  }),
+                        LinkButton(icon: SizedBox(child: Image.asset('assets/iconLink/android.png', 
+                       
+                         fit: BoxFit.cover)), text: 'Download for android', ontap: () {  }),
+                         LinkButton(icon: SizedBox(child: Image.asset('assets/iconLink/windows.png',
+                         width: 50,
+                       height: 50,
+                          fit: BoxFit.cover)), text: 'Download for windows', ontap: () {  }),
+                    ]
+                  ),
+                ))
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class LinkButton extends StatelessWidget {
+  final Widget icon;
+  final VoidCallback ontap;
+  final String text;
+  const LinkButton({Key? key, required this.icon, required this.ontap, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        height: 48.0,
+        child: Container(
+        
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(
+                Radius.circular(8.0),
+              ),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    offset: Offset(1.0, 2.0),
+                    blurRadius: 8.0,
+                    spreadRadius: 2.0)
+              ]),
+          child: Stack(
+            children: [
+              Row(
+                children: [
+                  Container(
+                      width: 48.0,
+                      height: 48.0,
+                      alignment: Alignment.centerLeft,
+                      decoration: const BoxDecoration(
+                     
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(8.0),
+                        ),
+                      ),
+                      child:  Align(
+                          alignment: Alignment.center,
+                          child: icon)),
+                  Expanded(
+                      child: Center(
+                    child: Text(text,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline6!
+                            .copyWith(color: Colors.black)),
+                  )),
+                ],
+              ),
+              SizedBox.expand(
+                child: Material(
+                  type: MaterialType.transparency,
+                  child: InkWell(onTap: ontap),
+                ),
+              ),
+            ],
           ),
-          ),
+        ),
+      ),
     );
   }
 }
